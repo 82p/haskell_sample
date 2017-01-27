eef76a9e25a76fa9daeaa14a052c490547161339
wataru haniyama <wataru.haniyama@gmail.com>
2017-01-27 21:24:14 +0900

    fix runhaskel.bat
    change to stack runhaskell and add command for misconversion

--- runhaskell.bat
+++ runhaskell.bat
@@ -1,14 +1,7 @@
 @echo off
 
+chcp 65001 > nul
+
 if "%~dpnx1" equ "" goto :eof
 
-setlocal
-set tempfile=%date:~4%%time::=%
-set tempfile=%tempfile:/=%
-set tempfile=%tempfile:.=%
-set tempfile=%tempfile: =%
-ghc -o %tempfile%.exe --make "%~dpnx1" -outputdir "tmp"
-%tempfile%.exe
-del %tempfile%.exe
-rd /s /q "tmp"
-endlocal
+stack runghc "%~dpnx1"
