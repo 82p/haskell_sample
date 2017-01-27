a4f8c5992d716b291932daf9cf49cf63cf1b36ad
wataru haniyama <wataru.haniyama@gmail.com>
2017-01-27 23:33:19 +0900

    comment change language
    japanese to english

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,31 @@
---初めての再帰関数
-add 1 = 1
-add x = x +  add (x - 1)
+--it's first time recursive function
+series 1 = 1
+series x = x +  series (x - 1)
 
---リストの操作
--- first (x:_) = x;
 
---リスト関数の再定義
---length' [] = 0
+{--this is comment
 
---sum'
-
---take'
-
---drop'
-
---reverse'
-
---whereを使ってみる
---addsub x y = (add,sub)
-
-{- ax + by = c と  (p,q)の交点
-   -- ax + by = c
-   -- ax - by = ap - bq
-   の連立方程式の解
--}
---prePoint p q a b c = (x,y)
+TODO LIST
+-- haskell list
+first (x:_) = x;
+--Redefine list functions
+length'
+sum' 
+take' 
+drop' 
+reverse' 
+--use where
+addsub x y = (add,sub)
+--math practice
+A straight line passing through (p,q)
+and cross at right angles the line ax + by = c.
+Find the intersection of thease lines. 
+-- prePoint p q a b c = (x,y)
+--}
 
 main = do
- print $ add 5
+ print $ series 5
+
 -- print $ first [1..5]
 -- print $ length' [1..5]
 -- print $ sum' [1..5]
@@ -37,7 +35,7 @@ main = do
 -- print $ reverse' [1..5]
 -- print $ addsub 5 3
  
--- letも使ってみる
+-- try let
 -- let (add,_) = addsub 5 3
 -- print add
 
